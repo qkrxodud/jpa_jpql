8fe3df2ff921411ebba1746cb26a323663ae094a
qkrxodud00 <qkrxodud00@gmail.com>
2022-04-04 17:17:59 +0900

    *프로젝션 - 여러 값 조회

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,10 +19,15 @@ public class JpaMain {
             member.setUsername("member1");
             em.persist(member);
 
-            Member memberResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("SingleResult = " + memberResult.getUsername());
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m ", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = resultList.get(0);
+            System.out.println(memberDTO.getUsername());
+            System.out.println(memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e) {
