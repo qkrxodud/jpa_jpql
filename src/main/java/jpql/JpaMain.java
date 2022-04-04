6d3357e74e512304fb5d9ea82a8fd98783835625
qkrxodud00 <qkrxodud00@gmail.com>
2022-04-04 15:30:01 +0900

    *기본 맵핑 연결

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,34 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 애플리케이션 에서 한개만 만들어 져야된다.
+
+        EntityManager em = emf.createEntityManager(); //하나의 단위를 만들때마다 만들어 줘야된다.
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+
+}
